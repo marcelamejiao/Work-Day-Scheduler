ccec7f50616e7874b1dd15a7c7355247d4bcb111
Marcela Mejia <mejiao24@hotmail.com>
2022-06-19 14:17:55 +1000

    Render the timeblocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,34 @@ function init () {
     renderTimeBlocks();
 }
 
+// display the bussiness hours (time blocks)
 function renderTimeBlocks () {
     var sampleRow = $('#sample-row');
     var timeBlocks = $('#time-blocks');
-    sampleRow.clone().appendTo(timeBlocks); 
+
+    var startHour = 9;
+    var endTime = 17;
+
+    for (var i = startHour ; i <= endTime; i++) {
+        // Create a new timeblock
+        var newRow = sampleRow.clone();
+        
+        // Select the column with the hour
+        var hourColumn = newRow.children('.hour');
+        
+        // Display time in AM/PM format
+        hourColumn.text(moment().hour(i).minutes(0).format('h a'));
+
+        // Add to the list of timeBlocks
+        newRow.appendTo(timeBlocks);
+    }
 }
 
 init();
+
+// display the bussiness hours (time blocks)
+// display the current time (green for current, gray(?) for past)
+// option to add the tasks
+// save button
+// json to save the data in local storage
+// colours the tasks
